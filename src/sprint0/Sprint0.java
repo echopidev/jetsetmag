a211db8f2900f99e94092788d9e656915d0c3078
ferchichi <ferchichi@ferchichi-PC>
2014-02-18 17:49:21 +0100

    new Commit by mamadou

---
+++ src/sprint0/Sprint0.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprint0;
+
+/**
+ *
+ * @author ferchichi
+ */
+public class Sprint0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
